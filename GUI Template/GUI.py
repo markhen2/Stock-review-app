fd097cd0d87c069aa09ff17558cd5313140664d0
Markhen2 <markhen2004@gmail.com>
2024-09-17 20:35:01 +0100

    Updated different windows for the GUI
    DCF class needs work!

--- GUI Template/GUI.py
+++ GUI Template/GUI.py
@@ -3,31 +3,62 @@ import tkinter as tk
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import fetch_basic_data
+#import fetch_basic_data
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import Toplevel
 
-dcf=""
-news=''
-fundamentals=''
-stock_data = fetch_basic_data.StockData('AAPL')
+#stock_data = fetch_basic_data.StockData('AAPL')
+
+#DCF Class logic
+
+#News Class logic (SOURCE FROM PROXY DATABASE)
+
+#ARI Suggestions and past buys import
+
+#Fundamentals Class logic
+
+
+#technical analysis class logic
 
 
 #def preprocess(ticker,report_question):
-    
 
-def run_report():
-    ticker=ticker.get()
-    #report_question=report.get()
 
-    #preprocess(dcf,news,fundamentals)
+
+def close_report():
     app.destroy()
+
+def search_ticker():
+    global ticker
+    ticker=ticker_input.get()
+    ticker_info_window=Toplevel(app)
+    ticker_info_window.title('Ticker Information')
+    ticker_info_window.geometry('800x600')
+    
+    customtkinter.CTkLabel(ticker_info_window,text='Stock Analysis').grid(row=0,column=1)
+
 
 customtkinter.set_appearance_mode('dark')
 app=customtkinter.CTk()
-app.geometry('800x600')
+app.geometry('200*300')
 app.title('Stock Analysis')
+
+
+def run_dcf():
+    dcf_window=Toplevel(app)
+    dcf_window.title('DCF Analysis')
+    dcf_window.geometry('800x600')
+
+    Label(dcf_window,text='DCF Analysis').pack()
+
+
+
+customtkinter.CTkButton(app,text='Open DCF Analysis' , command=run_dcf).grid(row=4,column=2)
+
 
 customtkinter.CTkLabel(app,text='input a ticker here please').grid(row=0,column=0)
 ticker_input=tk.Entry(app, width=50)
 ticker_input.grid(row=0,column=1)
-customtkinter.CTkButton(app,text='submit',command=run_report).grid(row=0,column=2)
+customtkinter.CTkButton(app,text='submit',command=search_ticker).grid(row=0,column=2)
 app.mainloop()
