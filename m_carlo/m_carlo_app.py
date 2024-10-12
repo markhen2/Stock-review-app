db19c109f141a456b5ab45f98b1ddfb430fa72a2
Markhen2 <markhen2004@gmail.com>
2024-10-12 15:08:06 +0100

    upadted M-carlo app

--- m_carlo/m_carlo_app.py
+++ m_carlo/m_carlo_app.py
@@ -103,7 +103,7 @@ def monte_carlo(ticker, prediction_days, number_simulations, BSM=0.26, threshold
                         f'P({threshold2:.2f} - {threshold1:.2f}): {between_thresholds_prob:.2%}',
             transform=ax.transAxes, fontsize=12,
             verticalalignment='bottom', bbox=props)
-
+    
     ax.annotate(f'{median_price_path[-1]:.2f}', xy=(days_to_forecast - 1, median_price_path[-1]), xycoords='data',
                 xytext=(15, 0), textcoords='offset points', color='blue', fontsize=12)
     ax.annotate(f'{lower_bound_68[-1]:.2f}', xy=(days_to_forecast - 1, lower_bound_68[-1]), xycoords='data',
@@ -117,8 +117,10 @@ def monte_carlo(ticker, prediction_days, number_simulations, BSM=0.26, threshold
 
     return fig
 
+
 def run_simulation():
     global ticker
+    global fig
     ticker = ticker_entry.get()
     prediction_days = int(prediction_days_entry.get()) 
     number_simulations = int(number_simulations_entry.get()) 
@@ -137,6 +139,12 @@ def tutorial():
     tutorial_window.geometry('900x700')
     ttk.Label(tutorial_window, text=f'Monte Carlo Simulatuon Tutorual\n\n 1)Enter a ticker (Find on Yahoo finance)\n\n 2)Enter the amount of days you want to project the shareprice\n Enter the amount of times you want to run the simulation (recommend 500)\n\n3)You must then enter the BSM.\n This is the implied volatility you are assuming\nYou should try different volatilities between 0 and 1 and see how the simulation reacts\nYou can find the actual implied volatility of your stock by looking for the OPtions chain on yahoo finance or bloomberg\n\n4) Once you have pressed run you will see the simulation graphic, you can still update all of the inputs and view the changes\n\n\n\nIf you have any questions please contact Mark Henry', foreground='black').grid(row=2, column=0,sticky='w')
 
+def save_figure():
+    global fig
+    file_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png"), ("All files", "*.*")])
+    if file_path:
+        fig.savefig(file_path)
+
 window = tk.Tk()
 window.title("Monte Carlo Simulation")
 
@@ -146,6 +154,8 @@ frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
 tutorial_button = ttk.Button(frame, text="Read tutorial", command=tutorial)
 tutorial_button.grid(row=0, column=2, columnspan=1)
 
+save_button = ttk.Button(frame, text="Save Figure", command=save_figure)
+save_button.grid(row=7, column=0, columnspan=1)
 
 ttk.Label(frame, text="Ticker (Required):").grid(row=0, column=0, sticky=tk.W)
 ticker_entry = ttk.Entry(frame)
